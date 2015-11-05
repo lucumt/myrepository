3edd591a9c79c17a5c426a7cc217350a0bf89f67
Rosen Lu <lucumt@gmail.com>
2015-11-05 23:16:19 +0800

    Modify code to prevent insert duplicate data.

--- python/alenspider/src/crackingcore/spider.py
+++ python/alenspider/src/crackingcore/spider.py
@@ -10,6 +10,7 @@ import uuid
 import logging
 import time
 import traceback
+import sys
 
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -21,14 +22,14 @@ from sqlalchemy import exc
 
 from models import Topic,Post
 
-concurrent=10
-q = Queue(concurrent*2)
+crackconcurrent=10
+crackq = Queue(crackconcurrent*2)
 
-# logfilename = 'crackinglog_'+datetime.now().strftime("%Y-%m-%d")+'.log'
+#logfilename = 'crackingcore_'+sys.argv[1]+'_'+datetime.now().strftime("%Y_%m_%d")+'.log'
 logging.basicConfig(name='crackinglog',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
 request = requests.Session()
-request.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+request.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.26'
 adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
 request.mount('http://', adapter)
   
@@ -56,7 +57,12 @@ def parse_module():
     eles = soup.findAll('div', id=re.compile('^category_\d+'))
     for ele in eles:
         itemid = ele['id']
-        if itemid in['category_1','category_5','category_67','category_10','category_154','category_15']:
+        if sys.argv[1] =='front':
+            items=['category_1','category_5','category_10','category_67']
+        else:
+            items=['category_154','category_15']
+        #if itemid in['category_1','category_5','category_67','category_10','category_154','category_15']:
+        if itemid in items:
             items = soup.find('div',id = itemid).findAll('h4',{'class':'forum_name'})
             for item in items:
                 url = item.strong.a['href']
@@ -70,8 +76,8 @@ def parse_module():
                 #parse topic in each page       
                 for i in range(totalpage,0,-1):
                     posturl = url+'page-'+str(i)+'?prune_day=100&sort_by=Z-A&sort_key=last_post&topicfilter=all'
-                    q.put(posturl)
-            q.join()
+                    crackq.put(posturl)
+            crackq.join()
             
     
 def parse_subforum_topic(url):
@@ -84,7 +90,7 @@ def parse_subforum_topic(url):
             totalpage = parse_totalpage(url)
             for i in range(totalpage,0,-1):
                 posturl = url+'page-'+str(i)+'?prune_day=100&sort_by=Z-A&sort_key=last_post&topicfilter=all'
-                q.put(posturl)
+                crackq.put(posturl)
 
 def parse_topics(url):
     logging.info('-------------------parsing page:%s',url)
@@ -97,41 +103,36 @@ def parse_posts(url):
     
     totalpage = parse_totalpage(url)
     
-    Session = sessionmaker()
-    Session.configure(bind= engine)
-    session = Session()
-    
-    addtopic = True
-    addpost = True
     topic = None
     
     topicuuid = None
     topictitle = None
     
     try:
-        postlists = []
         for i in range(totalpage,0,-1):
             
-            if not addpost:
-                break
+            time.sleep(10)
+            Session = sessionmaker()
+            Session.configure(bind= engine)
+            session = Session()
             
             postsurl = url+'page-'+str(i)
-#             soup2 = BeautifulSoup(request.get(postsurl).content,'html.parser')
+            postlists = []
+            soup2 = BeautifulSoup(request.get(postsurl).content,'html.parser')
             
             if i == totalpage:#parse the title
-                soup2 = BeautifulSoup(request.get(postsurl).content,'html.parser')
                 thirdpartyid=crackingpattern.match(url).group(1)
                 existstopics =session.query(Topic).filter(Topic.third_party_id==thirdpartyid).filter(Topic.url==url).all()
                 if existstopics:
                     topic = existstopics[0]
                     topicuuid = topic.uuid
-                    addtopic = False
+                    topictitle = topic.name
                 else:
                     topicuuid = str(uuid.uuid4())
                     topictitle = soup2.find('h1',{'class':'ipsType_pagetitle'}).getText().strip()
-                    logging.info('+++++++++++++++++++add topic++++++++++++++++++++'+url)
+                    logging.info('---------------------------add topic------------------'+postsurl)
                     logging.info('topic title:\t'+topictitle)
-                    logging.info('\turl:\t'+url)
+                    logging.info('url:\t'+url)
                     logging.info('third party id:\t'+str(thirdpartyid))
                     createdate = soup2.find('span',{'itemprop':'dateCreated'}).getText().strip()
                     if 'Today,' in createdate:
@@ -148,47 +149,31 @@ def parse_posts(url):
             for postele in posteles:
                 postid = postele['id'][8:]
                 membername = postele.find('span',{'itemprop':'creator name'}).getText().strip()
-                posttime = datetime.strptime(postele.find('abbr',{'itemprop':'commentTime'})['title'], "%Y-%m-%dT%H:%M:%S+00:00")
-                 
-                postnum = session.query(Post).filter(Post.pid==postid).filter(Post.created_at==posttime).filter(Post.member_name==membername).count()
+                postnum = session.query(Post).filter(Post.third_party_id==postid).filter(Post.member_name==membername).count()
+                
                 if postnum >0:
-                    addpost = False
+                    logging.info('------------------exists post ----------\t'+postid)
+                    break
                 else:
-                    bodystr = None
-                    commenttext = postele.find('div',{'class':'post_body'}).find('div',{'itemprop':'commentText'})
-                    bodies = commenttext.findAll('p',recursive=False)
-                    for body in bodies:
-                        if bodystr == None:
-                            bodystr = body.getText().strip()
-                        else:
-                            bodystr +='\n' + body.getText().strip()
-                    if not bodystr:
-                        bodystr = commenttext.getText().strip()
+                    posttime = datetime.strptime(postele.find('abbr',{'itemprop':'commentTime'})['title'], "%Y-%m-%dT%H:%M:%S+00:00")
+                    postbody = postele.find('div',{'itemprop':'commentText'}).getText().strip()
                     logging.info("------------------create post ----------\t"+postid)
                     logging.info("post third party id:\t"+postid)
                     logging.info("post member name:\t"+membername)
-                    logging.info("post body length:\t"+str(len(bodystr)))
+                    logging.info("post body length:\t"+str(len(postbody)))
                     logging.info("post create time:\t"+str(posttime))
-                    post = Post(uuid=str(uuid.uuid4()),thread_uuid=topicuuid,third_party_id=postid,member_name=membername,body=bodystr,created_at=posttime)
+                    post = Post(uuid=str(uuid.uuid4()),thread_uuid=topicuuid,third_party_id=postid,member_name=membername,body=postbody,created_at=posttime)
                  
                 if postnum == 0:
                     postlists.append(post)
-        if topic:
-            logging.info("Finished parse topic:\t"+topic.name+'\t<=========>\t'+url+'\ttotal posts:\t'+str(len(postlists)))
-            if addtopic:
-                session.add(topic)
-        else:
-            logging.info('++++++++++++++++++++++++++no add topic:\t%s',url)
-         
-        if postlists:
-            session.bulk_save_objects(postlists)
-        else:
-            logging.info('++++++++++++++++++++++++++no add posts,url:\t%s',url)
-         
-        if addtopic or postlists:
-            session.flush()
-            session.commit()
-        session.close()
+             
+            if postlists:
+                session.bulk_save_objects(postlists)
+                session.flush()
+                session.commit()
+                session.close()
+            else:
+                logging.info('++++++++++++++++++++++++++++no add posts,url:\t%s',postsurl)
         
     except exc.InvalidRequestError:
         logging.info('*********************add failed,the url is:\t%s',url)
@@ -198,19 +183,20 @@ def parse_posts(url):
   
 def dowork():
     while True:
-        url = q.get()
+        url = crackq.get()
         logging.info('++++++++++++++++++begin to processing url\t %s',url)
         parse_topics(url)
-        q.task_done()
+        crackq.task_done()
         time.sleep(2) 
 
 if __name__=="__main__":
     starttime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    for i in range(concurrent):
+    for i in range(crackconcurrent):
         t = Thread(target=dowork)
         t.daemon = True
         t.start()
+        time.sleep(5)
     parse_module()
     endtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print '********start time:\t',starttime
-    print '********end time:\t',endtime
+    logging.info('********start time:\t'+starttime)
+    logging.info('********end time:\t'+endtime)
