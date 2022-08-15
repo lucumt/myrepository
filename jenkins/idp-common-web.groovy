d9747e05996e344b27d4e9acb19b61974cb86c05
lucumt <lucumt@gmail.com>
2022-08-15 19:51:09 +0800

    添加流水线脚本

---
+++ jenkins/idp-common-web.groovy
@@ -0,0 +1,111 @@
+pipeline {
+  agent {
+    node {
+      label 'nodejs'
+    }
+
+  }
+  stages {
+    stage('初始阶段') {
+      parallel {
+        stage('拉取代码') {
+          agent none
+          steps {
+            git(credentialsId: 'gitlab-account', url: 'http://gitlab.xxx.com/idp-group/idp-common-web.git', branch: '$BRANCH_NAME', changelog: true, poll: false)
+            sh '''ls -al
+echo “拉取代码成功“'''
+          }
+        }
+
+        stage('识别系统环境') {
+          agent none
+          steps {
+            script {
+              switch(PRODUCT_PHASE) {
+                case “sit“:
+                env.NODE_PORT = 13000
+                env.CONFIG_FILE = “.env.sit“
+                break
+                case “test“:
+                env.NODE_PORT = 14000
+                env.CONFIG_FILE = “.env.test“
+                break
+                case “prod“:
+                env.NODE_PORT = 15000
+                env.CONFIG_FILE = “.env.production“
+                break
+              }
+              print(env.NODE_PORT)
+              env.BUILD_TAG = new Date().format(“yyyyMMdd-HHmmss“)
+            }
+
+          }
+        }
+
+      }
+    }
+
+    stage('端口替换') {
+      agent none
+      steps {
+        sh '''sed -i “s/10.30.5.150/10.30.5.170/g“ $CONFIG_FILE
+
+cat $CONFIG_FILE'''
+      }
+    }
+
+    stage('项目编译') {
+      agent none
+      steps {
+        container('nodejs') {
+          sh '''npm install node@16.13.1 --registry https://mirrors.hirain.com/repository/NPM/
+
+# 根据不同阶段进行编译构建
+npm run build:${PRODUCT_PHASE}'''
+        }
+
+      }
+    }
+
+    stage('镜像构建') {
+      agent none
+      steps {
+        container('nodejs') {
+          sh 'docker build -f kubesphere/Dockerfile -t idp-common-web:$BUILD_TAG .'
+        }
+
+      }
+    }
+
+    stage('镜像推送') {
+      agent none
+      steps {
+        container('nodejs') {
+          withCredentials([usernamePassword(credentialsId : 'harbor-account' ,usernameVariable : 'DOCKER_USER_VAR' ,passwordVariable : 'DOCKER_PWD_VAR' ,)]) {
+            sh 'echo “$DOCKER_PWD_VAR“ | docker login $REGISTRY -u “$DOCKER_USER_VAR“ --password-stdin'
+            sh '''docker tag  idp-common-web:$BUILD_TAG $REGISTRY/$DOCKERHUB_NAMESPACE/idp-common-web:$BUILD_TAG
+docker push  $REGISTRY/$DOCKERHUB_NAMESPACE/idp-common-web:$BUILD_TAG'''
+          }
+
+        }
+
+      }
+    }
+
+    stage('部署到dev') {
+      agent none
+      steps {
+        kubernetesDeploy(enableConfigSubstitution: true, deleteResource: false, kubeconfigId: 'idp-kubeconfig', configs: 'kubesphere/deploy.yml')
+      }
+    }
+
+  }
+  environment {
+    DOCKER_CREDENTIAL_ID = 'harbor-id'
+    GITHUB_CREDENTIAL_ID = 'gitlab-id'
+    KUBECONFIG_CREDENTIAL_ID = 'idp-kubeconfig'
+    REGISTRY = '10.30.5.170:30005'
+    DOCKERHUB_NAMESPACE = 'idp-library'
+    GITHUB_ACCOUNT = 'kubesphere'
+  }
+}
---
+++ jenkins/idp-system.groovy
@@ -0,0 +1,150 @@
+pipeline {
+  agent {
+    node {
+      label 'maven'
+    }
+
+  }
+  stages {
+    stage('default-0') {
+      parallel {
+        stage('拉取代码') {
+          agent none
+          steps {
+            git(credentialsId: 'gitlab-account', url: 'http://gitlab.xxx.com/idp-group/system.git', branch: '$BRANCH_NAME', changelog: true, poll: false)
+            sh '''sed -i “s/10.30.5.150/10.30.5.170/g“ app/src/main/resources/bootstrap.yml
+echo “拉取代码成功“'''
+          }
+        }
+
+        stage('识别系统环境') {
+          agent none
+          steps {
+            script {
+              switch(PRODUCT_PHASE) {
+                case “sit“:
+                env.NODE_PORT = 13002
+                break
+                case “test“:
+                env.NODE_PORT = 14002
+                break
+                case “prod“:
+                env.NODE_PORT = 15002
+                break
+              }
+            }
+
+          }
+        }
+
+      }
+    }
+
+    stage('单元测试') {
+      agent none
+      steps {
+        container('maven') {
+          sh '''mvn clean compile test
+echo “执行相关的单元测试“'''
+          script {
+            env.PROJECT_VERSION = sh(script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true)
+            env.BUILD_TIME = new Date().format(“yyyyMMdd-HHmmss“)
+            env.BUILD_TAG = PROJECT_VERSION + “-“ + BUILD_TIME
+          }
+
+        }
+
+      }
+    }
+
+    stage('代码分析') {
+      agent none
+      steps {
+        container('maven') {
+          withCredentials([string(credentialsId : 'idp-sonar-token' ,variable : 'SONAR_TOKEN' ,)]) {
+            withSonarQubeEnv('sonar') {
+              sh '''echo $PROJECT_VERSION
+mvn sonar:sonar \\
+  -Dsonar.projectKey=idp-system \\
+  -Dsonar.host.url=http://10.30.5.170:32387 \\
+  -Dsonar.login=$SONAR_TOKEN'''
+            }
+
+          }
+
+          timeout(unit: 'HOURS', activity: true, time: 1) {
+            waitForQualityGate 'false'
+          }
+
+        }
+
+      }
+    }
+
+    stage('项目编译') {
+      agent none
+      steps {
+        container('maven') {
+          sh 'ls'
+          sh 'mvn clean compile package -Dmaven.test.skip=true'
+        }
+
+      }
+    }
+
+    stage('制品保存') {
+      agent none
+      steps {
+        container('maven') {
+          archiveArtifacts 'app/target/*.jar'
+        }
+
+      }
+    }
+
+    stage('镜像构建') {
+      agent none
+      steps {
+        container('maven') {
+          sh '''docker build -f kubesphere/Dockerfile \\
+-t idp-system:$BUILD_TAG  \\
+--build-arg  PROJECT_VERSION=$PROJECT_VERSION \\
+--build-arg  NODE_PORT=$NODE_PORT \\
+--build-arg PRODUCT_PHASE=$PRODUCT_PHASE .'''
+        }
+
+      }
+    }
+
+    stage('镜像推送') {
+      agent none
+      steps {
+        container('maven') {
+          withCredentials([usernamePassword(credentialsId : 'harbor-account' ,usernameVariable : 'DOCKER_USER_VAR' ,passwordVariable : 'DOCKER_PWD_VAR' ,)]) {
+            sh 'echo “$DOCKER_PWD_VAR“ | docker login $REGISTRY -u “$DOCKER_USER_VAR“ --password-stdin'
+            sh '''docker tag  idp-system:$BUILD_TAG $REGISTRY/$DOCKERHUB_NAMESPACE/idp-system:$BUILD_TAG
+docker push  $REGISTRY/$DOCKERHUB_NAMESPACE/idp-system:$BUILD_TAG'''
+          }
+
+        }
+
+      }
+    }
+
+    stage('部署到dev') {
+      agent none
+      steps {
+        kubernetesDeploy(enableConfigSubstitution: true, deleteResource: false, kubeconfigId: 'idp-kubeconfig', configs: 'kubesphere/deploy.yml')
+      }
+    }
+
+  }
+  environment {
+    DOCKER_CREDENTIAL_ID = 'harbor-id'
+    GITHUB_CREDENTIAL_ID = 'gitlab-id'
+    KUBECONFIG_CREDENTIAL_ID = 'idp-kubeconfig'
+    REGISTRY = '10.30.5.170:30005'
+    DOCKERHUB_NAMESPACE = 'idp-library'
+    GITHUB_ACCOUNT = 'kubesphere'
+  }
+}
