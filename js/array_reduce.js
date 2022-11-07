9a1c50b571e698889c31c760718279aef4ecf817
卢运强 <lucumt@gmail.com>
2022-11-07 11:09:59 +0800

    提交对于reduce的改进用法

--- js/array_reduce.js
+++ js/array_reduce.js
@@ -7,16 +7,47 @@
         {  livingId: 996, cash: 10, tax: 0.06, isDisputed: true },
     ]
 
-    const result = orders.reduce((acc,val) => {
-	   let uniqueId = val.livingId
-	   let obj = acc.find(a => a.uniqueId == val.livingId)
-	   if(!!obj){
-	      let obj = acc.find(a => a.uniqueId == val.livingId)
-		  obj.numOrders++
-	   }else{
-	      acc.push({numOrders: 1, uniqueId: uniqueId})
-	   }
-	   return acc
-	},[])
+const result1 = orders.reduce((acc,val) => {
+   let uniqueId = val.livingId
+   let obj = acc.find(a => a.uniqueId == val.livingId)
+   if(!!obj){
+	  let obj = acc.find(a => a.uniqueId == val.livingId)
+	  obj.numOrders++
+   }else{
+	  acc.push({numOrders: 1, uniqueId: uniqueId})
+   }
+   return acc
+},[])
 
-    console.log(result)
+console.log(result1)
+console.log("-------------------------------")
+
+//---------------------------------------
+// https://stackoverflow.com/questions/74334249/how-to-sum-an-array-for-each-id-and-create-new-array-in-react/
+
+const data = [{"id": "One", "number": 100}, 
+  {"id": "One", "number": 150}, 
+  {"id": "One", "number": 200}, 
+  {"id": "Two", "number": 50}, 
+  {"id": "Two", "number": 100}, 
+  {"id": "Three", "number": 10}, 
+  {"id": "Three", "number": 90}]
+
+// 自己的写法
+let result2 = data.reduce((a, v) => {
+  let obj = a.find(i => i.id == v.id);
+  if (obj) {
+      obj.number += v.number;
+  } else {
+      a.push({...v});
+  }
+  return a;
+}, [])
+console.log(result2);
+console.log("-------------------------------")
+
+
+// 更简化写法
+let result3 = data.reduce((acc, {id, number}) => ({...acc, [id]: {id, number: acc[id] ? acc[id].number + number: number}}), {});
+console.log(Object.values(result3));
+
